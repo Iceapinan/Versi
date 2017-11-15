63a959be188966cf222624a3fbb009668027b1dc
Iceapinan <ice.ap2543@gmail.com>
2017-11-15 22:28:28 +0700

    Build TrendingRepoCell subclass and load dummy tableView data

--- Versi/Controller/TrendingFeedVC.swift
+++ Versi/Controller/TrendingFeedVC.swift
@@ -8,11 +8,31 @@
 
 import UIKit
 
-class TrendingFeedVC: UIViewController {
+class TrendingFeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "trendingRepoCell", for: indexPath) as? TrendingRepoCell else {
+            return UITableViewCell()
+        }
+        let repo = Repo(image: UIImage(named: "searchIconLarge")!, name: "SWIFT", description: "Apple's programming language.", numberOfForks: 356, language: "Swift", numberOfContributors: 1234, repoURL: "www.apple.com")
+        cell.configureCell(repo: repo)
+        return cell
     }
 
 
---
+++ Versi/View/TrendingRepoCell.swift
@@ -0,0 +1,55 @@
+//
+//  TrendingRepoCell.swift
+//  Versi
+//
+//  Created by IceApinan on 15/11/17.
+//  Copyright © 2017 IceApinan. All rights reserved.
+//
+
+import UIKit
+
+class TrendingRepoCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var repoImageView: UIImageView!
+    @IBOutlet weak var repoNameLabel: UILabel!
+    @IBOutlet weak var repoDescriptionLabel: UILabel!
+    @IBOutlet weak var numberOfForksLabel: UILabel!
+    @IBOutlet weak var languageLabel: UILabel!
+    @IBOutlet weak var contributorsLabel: UILabel!
+    @IBOutlet weak var backView: UIView!
+    @IBOutlet weak var viewReadMeButton: RoundedBorderButton!
+    
+    private var repoURL: String?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    
+    func configureCell(repo: Repo) {
+        repoImageView.image = repo.image
+        repoNameLabel.text = repo.name
+        repoDescriptionLabel.text = repo.description
+        numberOfForksLabel.text = String(describing: repo.numberOfForks)
+        languageLabel.text = repo.language
+        contributorsLabel.text = String(describing: repo.numberOfContributors)
+        repoURL = repo.repoURL
+    }
+    
+    override func layoutSubviews() {
+        backView.layer.cornerRadius = 15
+        backView.layer.shadowColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        backView.layer.shadowOpacity = 0.25
+        backView.layer.shadowRadius = 5.0
+        backView.layer.shadowOffset = CGSize(width: 0, height: 0)
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
